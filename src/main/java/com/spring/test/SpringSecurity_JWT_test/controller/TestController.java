1826db6391e85cb1c9cc7688ccf5d97c25c3bbfa
JecuMihaela <mihaela.nicoleta.jecu@gmail.com>
2023-01-10 11:41:05 +0200

    Set CrossOrigin to TestConstroller class

--- src/main/java/com/spring/test/SpringSecurity_JWT_test/controller/TestController.java
+++ src/main/java/com/spring/test/SpringSecurity_JWT_test/controller/TestController.java
@@ -1,12 +1,11 @@
 package com.spring.test.SpringSecurity_JWT_test.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bank/test")
+@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.POST},
+        allowCredentials = "false", allowedHeaders = {"Content-Type", "Authorization"})
 public class TestController {
     @GetMapping("/all")
     public String allAccess() {
